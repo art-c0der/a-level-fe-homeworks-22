6e2612179045520a7b15229b2bff0e87f7541cd2
Ann25@tmfv <kisilanna25@gmail.com>
2022-02-07 14:03:53 +0200

    add

--- students/Ann25tmfv/apple-shop/js/items.js
+++ students/Ann25tmfv/apple-shop/js/items.js
@@ -1272,36 +1272,4 @@ const items = [
     },
 ];
 
-const createCardContent = (object) => {
-    const {imgUrl, name, price,orderInfo} = object;
-
-    const card = `<div class ="card">
-    <button class="btn"></button>
-    <img class= "image" src="${imgUrl}" alt="${imgUrl}">
-    <p class="title">${name}</p>
-    <p class="inStock">${orderInfo.inStock} left in stock</p>
-    <p class="price">Price: ${price} $</p>
-    <button class="button">Add to cart</button>
-    <p class="reviews">${orderInfo.reviews}</p>
-    </div>
-    `;
-    return card;
-};
-
-const generateContent = (array) => {
-    const content = document.querySelector('#content');
-
-content.innerHTML = '';
-
-let data = '';
-
-array.forEach((element) => {
-data += createCardContent(element);
-});
-
-if (!data) data = 'No data';
-
-content.innerHTML = data;
-};
-
-generateContent(items);
+export default items;
