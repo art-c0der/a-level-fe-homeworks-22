8042c5e52c0bde8cbe73a3e617387b500e14c5cd
Gotem888 <Gotem888@gmail.com>
2022-02-07 15:03:17 +0200

    finished apple-shop1

--- students/gotem888/apple-shop/scripts/scripts.js
+++ students/gotem888/apple-shop/scripts/scripts.js
@@ -1,12 +1,13 @@
 import PRODUCTS from "./products.js";
 
+//--------Aside display------------
 let searchIcon = document.getElementById("search-icon");
 searchIcon.addEventListener("click", (e) => {
   let searchMenu = document.getElementById("aside__menu");
   if (searchMenu.style.display === "block") searchMenu.style.display = "none";
   else searchMenu.style.display = "block";
 });
-
+//-------- Slider ------------
 document.addEventListener("DOMContentLoaded", function () {
   var slider = new SimpleAdaptiveSlider(".slider", {
     loop: true,
@@ -15,9 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
     swipe: true,
   });
 });
-
+//-------- Accordion ------------
 const acc = document.getElementsByClassName("accordion");
-let i;
 
 const accArr = Array.from(acc);
 accArr.forEach((item) => {
@@ -32,13 +32,18 @@ accArr.forEach((item) => {
     }
   });
 });
-
+//-------- Create Card HTML------------
 const createCard = (object) => {
-  const { name, orderInfo, price, imgUrl } = object;
+  const { id, name, orderInfo, price, imgUrl } = object;
 
-  const card = `<div class="card">
+  const card = `<div class="card" id="${id}">
     
-    <input type="checkbox"  id="id" class="like">
+    <input type="checkbox"  id="id_${id}" class="like">
+    <label id="heart" class="heart" for="id_${id}" class="heartLabe"> 
+      <svg class="heartSvg" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path id="id${id}" class="heartSvg" fill="none" d="M9.12834 16.3219L9.12762 16.3213C6.97172 14.3663 5.22284 12.7794 4.0069 11.2928C2.7967 9.81324 2.16675 8.49409 2.16675 7.08333C2.16675 4.79281 3.95956 3 6.25008 3C7.54869 3 8.80287 3.60697 9.61941 4.56584L10.0001 5.01287L10.3808 4.56584C11.1973 3.60697 12.4515 3 13.7501 3C16.0406 3 17.8334 4.79281 17.8334 7.08333C17.8334 8.4941 17.2035 9.81327 15.9932 11.294C14.7782 12.7804 13.0314 14.3678 10.8782 16.3245L10.8732 16.329L10.8722 16.33L10.0014 17.1167L9.12834 16.3219Z" stroke="black"/>
+      </svg>
+    </label>
     <div class="product__img"><img src="${imgUrl}"></div>
     <p class="product__title">${name}</p>
     <span class="stock" id="inStock"><img src="${orderInfo.inStock.pict}"><p class="space">' '</p><p class="bold">${orderInfo.inStock.value}</p><p class="space">''</p>left in stock</span>
@@ -57,7 +62,7 @@ const createCard = (object) => {
     </div>`;
   return card;
 };
-
+//--------Generate Content from array ------------
 const generateContent = (array) => {
   const content = document.querySelector("#prodCard");
 
@@ -72,6 +77,7 @@ const generateContent = (array) => {
   if (!data) data = "Error 404 Not found";
   content.innerHTML = data;
 };
+//-------- Filter unAvalaible------------
 const filterUnAvalaib = PRODUCTS.reduce((arr, item) => {
   if (item.orderInfo.inStock.value < 1) {
     item.orderInfo.inStock.pict = item.orderInfo.inStock.pict.err;
@@ -86,21 +92,7 @@ const filterUnAvalaib = PRODUCTS.reduce((arr, item) => {
 
 generateContent(filterUnAvalaib);
 
-let elem = document.getElementsByClassName("like");
-
-for (let i = 0; i < elem.length; i++) {
-  let newId = "id_" + i;
-  let newIdsvg = "id" + i;
-  elem[i].outerHTML =
-    '<input type="checkbox" id="' +
-    newId +
-    '" class="like"> <label id="heart" class="heart" for="' +
-    newId +
-    '" class="heartLabe"> <svg class="heartSvg" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path id="' +
-    newIdsvg +
-    '" class="heartSvg" fill="none" d="M9.12834 16.3219L9.12762 16.3213C6.97172 14.3663 5.22284 12.7794 4.0069 11.2928C2.7967 9.81324 2.16675 8.49409 2.16675 7.08333C2.16675 4.79281 3.95956 3 6.25008 3C7.54869 3 8.80287 3.60697 9.61941 4.56584L10.0001 5.01287L10.3808 4.56584C11.1973 3.60697 12.4515 3 13.7501 3C16.0406 3 17.8334 4.79281 17.8334 7.08333C17.8334 8.4941 17.2035 9.81327 15.9932 11.294C14.7782 12.7804 13.0314 14.3678 10.8782 16.3245L10.8732 16.329L10.8722 16.33L10.0014 17.1167L9.12834 16.3219Z" stroke="black"/></svg></label>';
-}
-
+//-------- Like color changing------------
 let cardData = document.getElementsByClassName("card");
 const cardsArray = Array.from(cardData);
 cardsArray.forEach((card) => {
@@ -115,7 +107,7 @@ cardsArray.forEach((card) => {
         "#e06065";
   });
 });
-
+//--------Open and closing modal window ------------
 let modal = document.getElementById("myModal");
 
 cardsArray.forEach((card) => {
@@ -127,13 +119,80 @@ cardsArray.forEach((card) => {
     ) {
       e.stopPropagation();
     } else {
-      e.target = modal.style.display = "block";
+      modal.classList.toggle("close");
     }
   });
 });
 
 window.addEventListener("click", (event) => {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.classList.toggle("close");
   }
 });
+//-----------------------GENERATE MODAL--------------------------------------------------------------
+const createModalCard = (object) => {
+  const { name, orderInfo, price, imgUrl, color, os, chip, size } = object;
+
+  const modalCard = `<div class="modalCard">
+    <div class="modal__img"><img src="${imgUrl}"></div>
+    <div class="centralBlock">
+      <p class="product__title">${name}</p>
+      <div class="card-footer">
+        <div class="product-opinion">
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.99996 17.7917L8.79163 16.6917C4.49996 12.8 1.66663 10.2333 1.66663 7.08333C1.66663 4.51667 3.68329 2.5 6.24996 2.5C7.69996 2.5 9.09163 3.175 9.99996 4.24167C10.9083 3.175 12.3 2.5 13.75 2.5C16.3166 2.5 18.3333 4.51667 18.3333 7.08333C18.3333 10.2333 15.5 12.8 11.2083 16.7L9.99996 17.7917Z" fill="#F05454"/>
+          </svg>
+          <span class="review"><p class="bold">${orderInfo.reviews}%</p> Positive reviews <br> Above average</span>
+          <span class="orders"><p class="bold">${orderInfo.amount}</p><br> orders</span>
+        </div>
+      </div>
+      <div class="modalProductInfo">
+        <span>Color: <p class="bold">${color}</p></span>
+        <span>Operating system: <p class="bold">${os}</p></span>
+        <span>Chip: <p class="bold">${chip.name}</p></span>
+        <span>Height: <p class="bold">${size.height}</p></span>
+        <span>Width: <p class="bold">${size.width}</p></span>
+        <span>Depth: <p class="bold">${size.depth}</p></span>
+        <span>Weight: <p class="bold">${size.weight}</p></span>
+      </div>
+    </div>
+    <div class="modalPricePlace">
+      <span class="modalPrice"><p class="bold">$${price}</p></span>
+      <span class="modalStock" id="inStock"><img src="${orderInfo.inStock.pict}"><p class="space">' '</p>Stock: <p class="bold">${orderInfo.inStock.value}</p><p class="space">''</p>pcs.</span>
+      <a href="#" class="${orderInfo.inStock.class} + modalButton">Add to cart</a>
+    </div>
+  </div>`;
+  return modalCard;
+};
+//-------- generate Modal Content in click ------------
+const generateModalContent = (array) => {
+  const content = document.querySelector("#modal");
+
+  let data = "";
+  content.innerHTML = "";
+  let cardData = document.getElementsByClassName("card");
+  const cardsArray = Array.from(cardData);
+  cardsArray.forEach((card) => {
+    card.addEventListener("click", (e) => {
+      let itemId = "";
+      itemId = e.currentTarget.id;
+      array.forEach((el) => {
+        if (el.id == itemId) {
+          data = createModalCard(el);
+        }
+      });
+      if (!data) data = "Error 404 Not found";
+      content.innerHTML = data;
+    });
+  });
+};
+generateModalContent(filterUnAvalaib);
+//--------Aside display------------------------------
+//-------- Slider -----------------------------------
+//-------- Accordion --------------------------------
+//-------- Create Card HTML--------------------------
+//--------Generate Content from array ---------------
+//-------- Filter un Avalaible-----------------------
+//--------Open and closing modal window -------------
+//--------GENERATE MODAL-----------------------------
+//-------- generate Modal Content in click ----------
